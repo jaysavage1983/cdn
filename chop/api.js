ea742a56ba3f8d46dc81aa8e0bc74bff4f21994e
Jay Greentree <JayGreentree@users.noreply.github.com>
2022-04-23 23:58:38 -0400

    First commit

---
+++ chop/api.js
@@ -0,0 +1,35 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+/* PLEASE DO NOT COPY AND PASTE THIS CODE. */(function() {if (!window['___grecaptcha_cfg']) { window['___grecaptcha_cfg'] = {}; };if (!window['___grecaptcha_cfg']['render']) { window['___grecaptcha_cfg']['render'] = 'onload'; };window['__google_recaptcha_client'] = true;var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;po.src = 'https://web.archive.org/web/20170314193848/https://www.gstatic.com/recaptcha/api2/r20170307150823/recaptcha__en.js'; var elem = document.querySelector('script[nonce]');var nonce = elem && (elem['nonce'] || elem.getAttribute('nonce'));if (nonce) { po.setAttribute('nonce', nonce); }var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);})();
+
+}
+/*
+     FILE ARCHIVED ON 19:38:48 Mar 14, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:32:33 Apr 24, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.091
+  exclusion.robots.policy: 0.081
+  RedisCDXSource: 13.681
+  esindex: 0.009
+  LoadShardBlock: 603.902 (6)
+  PetaboxLoader3.datanode: 454.711 (8)
+  CDXLines.iter: 106.22 (3)
+  load_resource: 337.121 (2)
+  PetaboxLoader3.resolve: 156.936 (2)
+*/
