bec01354d573c1b9f93af5b93029efad434a4a72
Jay Greentree <JayGreentree@users.noreply.github.com>
2025-03-07 14:50:12 -0600

    Update and rename _pages_10_a8114a059d1dc0e5293cd46491fa7561.js to pages_10_a8114a059d1dc0e5293cd46491fa7561.js

--- web.archive.org_files/pages_10_a8114a059d1dc0e5293cd46491fa7561.js
+++ web.archive.org_files/pages_10_a8114a059d1dc0e5293cd46491fa7561.js
@@ -1,15 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
-
 
 /*
  * Modernizr JavaScript library 1.1
@@ -6832,26 +6820,3 @@ function getWindowOpener() {
 
     return window.opener;
 }
-
-
-}
-/*
-     FILE ARCHIVED ON 23:34:41 Dec 28, 2010 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 03:22:39 Mar 07, 2025.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.472
-  exclusion.robots: 0.016
-  exclusion.robots.policy: 0.007
-  esindex: 0.009
-  cdx.remote: 20.005
-  LoadShardBlock: 161.463 (3)
-  PetaboxLoader3.datanode: 107.954 (4)
-  PetaboxLoader3.resolve: 432.669 (3)
-  load_resource: 395.407
-*/
